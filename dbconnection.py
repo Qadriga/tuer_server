8db352c249f7ffc01954e52380a8058fea3717c1
Qadriga <rob.sandhausen@freenet.de>
2017-03-17 09:16:03 +0100

    added database query with mysql connection note: no query or tabel, coloums are created atm

--- dbconnection.py
+++ dbconnection.py
@@ -7,17 +7,43 @@ def check_user(RFID=[]):
     checks if the user is in the data base
     :return: True if the user is in the database False if not
     """
-    con = MySQLdb.connect('') # todo set connection string here
+    """
+    host
+      string, host to connect
+
+    user
+      string, user to connect as
+
+    passwd
+      string, password to use
+
+    db
+      string, database to use
+
+    port
+      integer, TCP/IP port to connect to
+
+    unix_socket
+      string, location of unix_s
+      """
+    con = MySQLdb.connect(host="127.0.0.1", user="pi" , db="tuer", passwd='raspberry')
     cur = con.cursor()
     try:
+        query = str()
+        query_elements = list()
         if not (isinstance(RFID,list)):
             return False
+        # cur.execute()
         # todo make string magik here
     except MySQLdb.Error, e:
-        pass
+        print(e.message)
     finally:
         if con:
             con.close()
 
 
 
+if __name__ == '__main__':
+    print("Checking database Connection")
+    v = check_user([0,0,0,0])
+    print(v)
