11d07da12da3ea7babc9b2772ed39e9aa2d45713
Qadriga <rob.sandhausen@freenet.de>
2018-08-01 21:49:10 +0200

    push latests changes

--- dbconnection.py
+++ dbconnection.py
@@ -9,8 +9,29 @@ except ImportError:
     pip.main(args=["install", "-r", "requirements.txt"])
     import MySQLdb
 
+import MySQLdb.cursors
 
-def check_user(RFID=[]):
+
+SQLPASSWRD = None
+DOORID = None
+
+
+def append_rfid(RFID=[]):
+    """
+
+    :param RFID: list of byte thats represents the RFID tag number
+    :return: a int or long value to
+    """
+    res = int()
+    if not isinstance(RFID, (tuple, list)):
+        return 0
+    RFID.reverse()
+    for elem in RFID:
+        res = res | (elem << (RFID.index(elem)*8))
+    return res
+
+
+def check_user(rfid=list()):
     """
     checks if the user is in the data base
     :return: True if the user is in the database False if not
@@ -35,32 +56,63 @@ def check_user(RFID=[]):
       string, location of unix_s
       """
     try:
-        con = MySQLdb.connect(host="127.0.0.1", user="pi", db="tuer", passwd='raspberry')
+        con = MySQLdb.connect(host="127.0.0.1",
+                              user="p52692",
+                              db="usr_p52692_2",
+                              passwd="zWg9iDkF",
+                              port=int(3307),
+                              cursorclass=MySQLdb.cursors.DictCursor)
+        # try to connect to the database on port 3307
+        # sshtunnel establish the connection thought the remote server
         cur = con.cursor()
-    except MySQLdb.Error,e:
-        print(e.message)
+    except MySQLdb.Error, e:
+        print(e)
         return False
     try:
-        query = str()
-        query_elements = list()
-        if not (isinstance(RFID, list)):
+        query_elements = list(("tag_id",str(DOORID)))
+        if not (isinstance(rfid, list)):
             return False
+        tag = append_rfid(rfid)
+        query_elements.append(str(tag))
+
         # bring the rfid into a string form
-        query = "SELECT * FROM tuer WHERE rfid=%s"
-        cur.execute(query, query_elements)
+        query = "SELECT %s FROM authorisations WHERE door_id=%s AND tag_id=%s" #
+        own_build = query % tuple(query_elements)
+        print own_build
+        cur.execute(own_build)
         result = cur.fetchall()
+
+        print result
         if result is not None:
-            return True
+            if not isinstance(result, (tuple, dict)):
+                return False
+            if long(tag) == result[0]['tag_id']:
+                return True
+            else:
+                return False
         # todo make string magik here
     except MySQLdb.Error, e:
-        print(e.message)
+        print(e)
         return False
     finally:
         if con:
             con.close()
 
 
+def read_conf():
+    global SQLPASSWRD
+    SQLPASSWRD = 'zWg9iDkF'
+    global DOORID
+    DOORID = '010'
+read_conf()
+
 if __name__ == '__main__':
     print("Checking database Connection")
-    v = check_user([0,0,0,0])
+    read_conf()
+    from time import time
+    start = time()
+    v = append_rfid([0xff, 0xfe, 0x02, 0x5, 0x1])
+    # v = check_user(['123','456','789','000'])
+    end = time()
+    print end - start
     print(v)
--- main.py
+++ main.py
@@ -4,10 +4,11 @@ main file of the RGH Door opening Project this implements the main loop
 
 
 # todo make wrapper class fot the rfid tags
-import signal
-import sys
-import dbconnection
-import RPi.GPIO as GPIO
+import signal # singal handelers
+import sys # system functions
+import dbconnection # database module
+import ssh_tunnel # ssh tunnel module
+import RPi.GPIO as GPIO #gpio module
 #import RPIO as GPIO
 import time
 import LCD_I2C.lcd as lcd
@@ -15,10 +16,16 @@ import RFID.MFRC522
 
 
 interrupt = None
+DOORPIN = 9
 RFID_Reader = RFID.MFRC522.MFRC522()
-lcd.initialize()
-lcd.clear()
 
+lcd.initialize()  # start the lcd display
+lcd.clear()
+#setup the gpio pins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(DOORPIN, GPIO.OUT)
+if ssh_tunnel.TUNNEL is None:
+    ssh_tunnel.SSHTunnelWrapper()  # start the ssh tunnel wrapper for the conection
 
 def shutdown(*args):
     """
@@ -45,6 +52,20 @@ def chip_check_isr(GPIO_id, value):
     """
     pass  # Dummy
 
+def open_door():
+    """
+    function witch is called to open the door by set one pin to 1 for a specific time
+    :return:
+    """
+    GPIO.output(DOORPIN,GPIO.UP)
+
+
+def lock_door():
+    """
+    locks the door by setting a pin to 0
+    :return:
+    """
+    GPIO.output(DOORPIN, GPIO.DOWN)
 
 def poll_signal():
     """
@@ -58,7 +79,7 @@ def poll_signal():
     lcd.noBacklight()
 
     while True:
-        time.sleep(0.1)
+        time.sleep(0.2)
         (status, TagType) = RFID_Reader.MFRC522_Request(RFID.MFRC522.MFRC522.PICC_REQIDL)
         if status == RFID_Reader.MI_OK:
             lcd.backlight()
@@ -66,14 +87,17 @@ def poll_signal():
             lcd.printString("Check RFID-Tag", 0)
             status, rfid = RFID_Reader.MFRC522_Anticoll()
             if status == RFID_Reader.MI_OK:
-                if dbconnection.check_user(rfid):  # this will return true if id is valid
+                if dbconnection.check_user(rfid):  # rfid should be an array of substrings or this functuon will fail
+                    #  this will return true if id is valid
                     lcd.printString("Welcome :-)", lcd.LINE_1)
                     lcd.printString("Door will open", lcd.LINE_2)
+                    open_door()
                     for i in range(0, 5):
                         lcd.noBacklight()
                         time.sleep(0.5)
                         lcd.backlight()
                         time.sleep(0.5)
+                    lock_door()
                 else:
                     lcd.printString("Invalid Tag", lcd.LINE_1)
                     lcd.printString("Door will not open", lcd.LINE_2)
@@ -94,5 +118,6 @@ signal.signal(signal.SIGINT, shutdown)  # attach the signal handler for shutdown
 try:
     poll_signal()
 except KeyboardInterrupt:
+    GPIO.cleanup()
     sys.exit(0)
 
